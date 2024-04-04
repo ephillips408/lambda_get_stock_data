5c7971a3eeb54907771b3fd5b24955b1bef3efa8
Eric Phillips <e.phillips.59715@gmail.com>
2024-04-04 09:02:38 -0700

    Updated symbols list to reference events/event.json

--- hello_world/app.py
+++ hello_world/app.py
@@ -8,6 +8,7 @@ from utils import (get_stock_data, clean_stock_data, batch_write_stocks)
 logger = logging.getLogger()
 logger.setLevel('INFO')
 
+
 def lambda_handler(event, context):
     """Sample pure Lambda function
 
@@ -32,29 +33,29 @@ def lambda_handler(event, context):
 
     # Initialize the DynamoDB table
     db_client = boto3.client('dynamodb')
-    
+
     # Get the stock data from the API
     api_response = get_stock_data(
-        api_key = os.environ['ALPHAVANTAGE_API_KEY'],
-        symbols = ['AMD', 'INTC']
+        api_key=os.environ['ALPHAVANTAGE_API_KEY'],
+        symbols=event['stocks']
     )
-    
+
     logger.info('Successfully return the data from the API')
-    
+
     # Clean the data that was returned from the API
     clean_data = clean_stock_data(
-        data = api_response
+        data=api_response
     )
-    
+
     logger.info('Successfully cleaned the data')
-    
+
     # Push the data to DynamoDB
     results = batch_write_stocks(
-        db_client = db_client,
-        table_name = os.environ['TABLE_NAME'],
-        stock_data = clean_data
+        db_client=db_client,
+        table_name=os.environ['TABLE_NAME'],
+        stock_data=clean_data
     )
-    
+
     logger.info('Successfully pushed the data to DynamoDB')
 
     return {
